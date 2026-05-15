b463c4eb64e8f18b3f226afac824c63d96d303bb
muteekhan06 <muteekhan06@gmail.com>
2026-05-15 13:35:40 +0500

    feat: complete Lahore dashboard with all 41 OLX areas, 50 brands, 8 fuel types, 11 body types, 28 features, 49 reg cities, 22 colors

--- app/config.py
+++ app/config.py
@@ -67,9 +67,34 @@ class ScraperConfig:
 # Define available locations for scraping
 # Define available locations for scraping
 LOCATIONS_LAHORE = {
+    "dha_defence": {
+        "name": "DHA Defence",
+        "url": "https://www.olx.com.pk/dha-defence_g9/cars_c84",
+        "enabled": True,
+    },
     "johar_town": {
         "name": "Johar Town",
         "url": "https://www.olx.com.pk/johar-town_g5000042/cars_c84",
+        "enabled": True,
+    },
+    "allama_iqbal_town": {
+        "name": "Allama Iqbal Town",
+        "url": "https://www.olx.com.pk/allama-iqbal-town_g5000002/cars_c84",
+        "enabled": True,
+    },
+    "gulberg": {
+        "name": "Gulberg",
+        "url": "https://www.olx.com.pk/gulberg_g7/cars_c84",
+        "enabled": True,
+    },
+    "bahria_town": {
+        "name": "Bahria Town",
+        "url": "https://www.olx.com.pk/bahria-town_g5000005/cars_c84",
+        "enabled": True,
+    },
+    "cantt": {
+        "name": "Cantt",
+        "url": "https://www.olx.com.pk/cantt_g5000047/cars_c84",
         "enabled": True,
     },
     "model_town": {
@@ -77,9 +102,44 @@ LOCATIONS_LAHORE = {
         "url": "https://www.olx.com.pk/model-town_g5000051/cars_c84",
         "enabled": True,
     },
-    "valencia_town": {
-        "name": "Valencia Town",
-        "url": "https://www.olx.com.pk/valencia-town_g5000081/cars_c84",
+    "sabzazar": {
+        "name": "Sabzazar",
+        "url": "https://www.olx.com.pk/sabzazar_g5000064/cars_c84",
+        "enabled": True,
+    },
+    "township": {
+        "name": "Township",
+        "url": "https://www.olx.com.pk/township_g5000078/cars_c84",
+        "enabled": True,
+    },
+    "wapda_town": {
+        "name": "Wapda Town",
+        "url": "https://www.olx.com.pk/wapda-town_g5000079/cars_c84",
+        "enabled": True,
+    },
+    "samanabad": {
+        "name": "Samanabad",
+        "url": "https://www.olx.com.pk/samanabad_g5000791/cars_c84",
+        "enabled": True,
+    },
+    "faisal_town": {
+        "name": "Faisal Town",
+        "url": "https://www.olx.com.pk/faisal-town_g5000024/cars_c84",
+        "enabled": True,
+    },
+    "gulshan_e_ravi": {
+        "name": "Gulshan-e-Ravi",
+        "url": "https://www.olx.com.pk/gulshan-e-ravi_g5000033/cars_c84",
+        "enabled": True,
+    },
+    "gt_road": {
+        "name": "GT Road",
+        "url": "https://www.olx.com.pk/gt-road_g5000475/cars_c84",
+        "enabled": True,
+    },
+    "shahdara": {
+        "name": "Shahdara",
+        "url": "https://www.olx.com.pk/shahdara_g5000069/cars_c84",
         "enabled": True,
     },
     "askari": {
@@ -87,9 +147,124 @@ LOCATIONS_LAHORE = {
         "url": "https://www.olx.com.pk/askari_g5000331/cars_c84",
         "enabled": True,
     },
-    "dha_defence": {
-        "name": "DHA Defence (Lahore)",
-        "url": "https://www.olx.com.pk/dha-defence_g9/cars_c84",
+    "valencia_town": {
+        "name": "Valencia Town",
+        "url": "https://www.olx.com.pk/valencia-town_g5000081/cars_c84",
+        "enabled": True,
+    },
+    "raiwind_road": {
+        "name": "Raiwind Road",
+        "url": "https://www.olx.com.pk/raiwind-road_g5000741/cars_c84",
+        "enabled": True,
+    },
+    "shadbagh": {
+        "name": "Shadbagh",
+        "url": "https://www.olx.com.pk/shadbagh_g5000068/cars_c84",
+        "enabled": True,
+    },
+    "jail_road": {
+        "name": "Jail Road",
+        "url": "https://www.olx.com.pk/jail-road_g5000539/cars_c84",
+        "enabled": True,
+    },
+    "al_rehman_garden": {
+        "name": "Al Rehman Garden",
+        "url": "https://www.olx.com.pk/al-rehman-garden_g5000314/cars_c84",
+        "enabled": True,
+    },
+    "mughalpura": {
+        "name": "Mughalpura",
+        "url": "https://www.olx.com.pk/mughalpura_g5000055/cars_c84",
+        "enabled": True,
+    },
+    "park_view_villas": {
+        "name": "Park View Villas",
+        "url": "https://www.olx.com.pk/park-view-villas_g5000709/cars_c84",
+        "enabled": True,
+    },
+    "thokar_niaz_baig": {
+        "name": "Thokar Niaz Baig",
+        "url": "https://www.olx.com.pk/thokar-niaz-baig_g5000077/cars_c84",
+        "enabled": True,
+    },
+    "bahria_orchard": {
+        "name": "Bahria Orchard",
+        "url": "https://www.olx.com.pk/bahria-orchard_g5000356/cars_c84",
+        "enabled": True,
+    },
+    "baghbanpura": {
+        "name": "Baghbanpura",
+        "url": "https://www.olx.com.pk/baghbanpura_g5000004/cars_c84",
+        "enabled": True,
+    },
+    "pak_arab_housing": {
+        "name": "Pak Arab Housing Society",
+        "url": "https://www.olx.com.pk/pak-arab-housing-society_g5000701/cars_c84",
+        "enabled": True,
+    },
+    "marghzar_officers": {
+        "name": "Marghzar Officers Colony",
+        "url": "https://www.olx.com.pk/marghzar-officers-colony_g5000621/cars_c84",
+        "enabled": True,
+    },
+    "harbanspura": {
+        "name": "Harbanspura",
+        "url": "https://www.olx.com.pk/harbanspura_g5000035/cars_c84",
+        "enabled": True,
+    },
+    "central_park": {
+        "name": "Central Park Housing Scheme",
+        "url": "https://www.olx.com.pk/central-park-housing-scheme_g5000383/cars_c84",
+        "enabled": True,
+    },
+    "chungi_amar_sadhu": {
+        "name": "Chungi Amar Sadhu",
+        "url": "https://www.olx.com.pk/chungi-amar-sadhu_g5000398/cars_c84",
+        "enabled": True,
+    },
+    "gajju_matah": {
+        "name": "Gajju Matah",
+        "url": "https://www.olx.com.pk/gajju-matah_g5000450/cars_c84",
+        "enabled": True,
+    },
+    "garden_town": {
+        "name": "Garden Town",
+        "url": "https://www.olx.com.pk/garden-town_g5000025/cars_c84",
+        "enabled": True,
+    },
+    "daroghewala": {
+        "name": "Daroghewala",
+        "url": "https://www.olx.com.pk/daroghewala_g5000010/cars_c84",
+        "enabled": True,
+    },
+    "ferozepur_road": {
+        "name": "Ferozepur Road",
+        "url": "https://www.olx.com.pk/ferozepur-road_g5000446/cars_c84",
+        "enabled": True,
+    },
+    "tajpura": {
+        "name": "Tajpura",
+        "url": "https://www.olx.com.pk/tajpura_g5000074/cars_c84",
+        "enabled": True,
+    },
+    "lda_avenue": {
+        "name": "LDA Avenue",
+        "url": "https://www.olx.com.pk/lda-avenue_g5000601/cars_c84",
+        "enabled": True,
+    },
+    "walton_road": {
+        "name": "Walton Road",
+        "url": "https://www.olx.com.pk/walton-road_g5000883/cars_c84",
+        "enabled": True,
+    },
+    "maulana_shaukat_ali_road": {
+        "name": "Maulana Shaukat Ali Road",
+        "url": "https://www.olx.com.pk/maulana-shaukat-ali-road_g5000624/cars_c84",
+        "enabled": True,
+    },
+    "taj_bagh": {
+        "name": "Taj Bagh",
+        "url": "https://www.olx.com.pk/taj-bagh_g5000073/cars_c84",
         "enabled": True,
     },
 }
